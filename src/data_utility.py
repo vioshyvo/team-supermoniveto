4856fd7621a11376232bcb328bea56d134d6116f
Ville Hyvonen <ville.o.hyvonen@helsinki.fi>
2017-12-03 17:37:06 +0200

    Wrap loading of topic index

--- src/data_utility.py
+++ src/data_utility.py
@@ -6,6 +6,11 @@ def times_two(x):
     return 2 * x
 
 def download_data(database_path = 'train/'):
+    """
+    Downloads the data set if it is not yet downloaded (the download path given as argument)
+    does not yet exist into the path given as an argument, and unzips the data set
+    if it is not yet unzipped.
+    """
     corpus_path = database_path + 'REUTERS_CORPUS_2/'
     data_path = corpus_path + 'data/'
     codes_path = corpus_path + 'codes/'
@@ -41,5 +46,35 @@ def download_data(database_path = 'train/'):
     else:
         print('Data set already unzipped.')
     
+
+def read_topics(database_path):
+    """
+    Read topics of the data set into the look - up table. Returns:
+    topics = topic codes as an array
+    topic_index = dictionary with topic code as key, and index of topic in topics as a value
+    topic_labels = dictionary with topic code as key, and topic labels as a value
+    """
+    topics = []
+    topic_labels = {}
+    corpus_path = database_path + 'REUTERS_CORPUS_2/'
+    codes_path = corpus_path + 'codes/'
+    codes_file = codes_path + 'topic_codes.txt'
+    with open(codes_file) as f:
+        for line in f:
+            if not line.startswith(';'):
+                splits = line.split()
+                topic_code = splits[0]
+                topic_labels[topic_code] = ' '.join(splits[1:len(splits)])
+                topics.append(topic_code)
+    
+    n_class = len(topics)
+    topic_index = {topics[i] : i for i in range(n_class)}
+    
+    return (topics, topic_index, topic_labels)
+
+
+
+
+
 
 
